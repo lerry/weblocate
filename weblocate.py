b1bfa250bf4e4771297f736f27e26998375e40a5
Lerry <lvdachao@gmail.com>
2012-01-28 17:48:48 +0800

    Add a function "recent search:" ,add a git ignore file

--- weblocate.py
+++ weblocate.py
@@ -8,7 +8,6 @@ Last edit at 2011-11-30 18:54
 Finish at 2011-11-30 21:30
 '''
 import os
-import time
 import web
 
 urls = (
@@ -17,22 +16,24 @@ urls = (
 )
 
 render = web.template.render('tmpl',)
+recent = []
 
 
 class index:
 
     def GET(self):
         results = ''
-        return render.index('','',results)
+        return render.index('','',results, recent)
 
     def POST(self):
         i = web.input()
         if i:
             keyword = i.keyword.encode('utf-8')
+            recent.append(keyword)
             result = os.popen('mlocate '+keyword).read().split('\n')
             length = len(result)-1
             if length == 0:
-                return render.index(keyword,'未找到匹配内容', [])
+                return render.index(keyword,'未找到匹配内容', [], '')
             results = []
             limit = 1000
             if length < limit:
@@ -40,7 +41,7 @@ class index:
             length = '有'+str(length)+'个文件匹配，显示前'+str(limit)+'个'
             for i in result:
                 results.append(i.decode('utf-8'))
-            return render.index(keyword,length, results[:limit])
+            return render.index(keyword,length, results[:limit], '')
 
 class get:
     def GET(self, url):
@@ -48,18 +49,18 @@ class get:
             try:
                 f = open(url,'rb').read()
                 #web.header('Content-type','application/octet-stream')
-               # web.header('Transfer-Encoding','chunked') 
+               # web.header('Transfer-Encoding','chunked')
                 return f#url.encode('utf-8')
             except:
-                return '文件读取失败'    
+                return '文件读取失败'
         else:
-            return '文件禁止访问'    
+            return '文件禁止访问'
         #print '^-^'
 
 def webheader():
     web.header('Content-Type', 'text/html')
-    
-app = web.application(urls,globals(),autoreload=True)       
+
+app = web.application(urls,globals(),autoreload=True)
 #app.add_processor(web.loadhook(webheader))
 
 if __name__ == '__main__':
