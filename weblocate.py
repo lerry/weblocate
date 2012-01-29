c3af2031c8458c099b56f0002f98afa76dbcdb74
Lerry <lvdachao@gmail.com>
2012-01-29 14:10:02 +0800

    dd new function to: show file list, add support for subpath

--- weblocate.py
+++ weblocate.py
@@ -10,30 +10,41 @@ Finish at 2011-11-30 21:30
 import os
 import web
 
+location = ''
+
+locate = ''
+if location:
+    locate = '/' + location
+
 urls = (
-   '/', 'index',
-   '^/get(.*)','get',
-)
+    '^'+locate+'/get(.*)', 'get',
+    locate+'(.*)', 'index')
+
+
+class reblog:
+    def GET(self): raise web.seeother('/')
+
 
 render = web.template.render('tmpl',)
 recent = []
 
-
 class index:
 
-    def GET(self):
+    def GET(self, arg):
         results = ''
-        return render.index('','',results, recent)
+        return render.index('','',results,location,recent)
 
-    def POST(self):
+    def POST(self, arg):
         i = web.input()
         if i:
             keyword = i.keyword.encode('utf-8')
-            recent.append(keyword)
+            global recent
+            recent = [keyword] + recent
+            #recent.append(keyword)
             result = os.popen('mlocate '+keyword).read().split('\n')
             length = len(result)-1
             if length == 0:
-                return render.index(keyword,'未找到匹配内容', [], '')
+                return render.index(keyword,'未找到匹配内容', [],location,'')
             results = []
             limit = 1000
             if length < limit:
@@ -41,27 +52,40 @@ class index:
             length = '有'+str(length)+'个文件匹配，显示前'+str(limit)+'个'
             for i in result:
                 results.append(i.decode('utf-8'))
-            return render.index(keyword,length, results[:limit], '')
+            return render.index(keyword,length, results[:limit], location, '')
 
 class get:
     def GET(self, url):
+        if not url:
+            url = '/'
         #print url.encode('utf-8')
         client_ip = web.ctx.ip
-        #check ip, if not trustable,it can only just get files in some path
-        if client_ip  in ['127.0.0.1','localhost'] or url.startswith(('/home')):
-            if not url.split('/')[1] in ['lib','root']:
-                try:
-                    f = open(url,'rb').read()
-                    return f
-                except:
-                    return '文件读取失败'
+        #print client_ip
+        if client_ip  in ['127.0.0.1','localhost'] or url.startswith(('/home/public')):
+            if os.path.isfile(url):
+                if not url.split('/')[1] in ['lib','root']:
+                    try:
+                        f = open(url,'rb').read()
+                        return f
+                    except:
+                        return '文件读取失败'
+            elif os.path.isdir(url):
+                temp_list = os.listdir(url)
+                #print chardet.detect(temp_list[1])
+                results = []
+                for i  in temp_list:
+                    results.append(i)
+                if url == '/':
+                    url = ''
+                return render.list(results, location, url)
         return '文件禁止访问'
 
 def webheader():
     web.header('Content-Type', 'text/html')
 
-app = web.application(urls,globals(),autoreload=True)
 #app.add_processor(web.loadhook(webheader))
 
-if __name__ == '__main__':
-    app.run()
+app_main = web.application(urls, globals())
+application = app_main.wsgifunc()
+if __name__=='__main__':
+    app_main.run()
